2e8d96b25a8c5a18f4c95caf792f39a9ee33736d
Steven Quiroa <stevenkm92@gmail.com>
2013-11-03 18:53:28 -0600

    Todo configurado y listo para usar.

--- main/views.py
+++ main/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import login, authenticate, logout	
+
+def home(request):
+	return render(request,'home.html')
