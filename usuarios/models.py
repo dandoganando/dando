97cdcec1c9ea85433e2ac473d3be6ad6e25dc87c
Steven Quiroa <stevenkm92@gmail.com>
2013-11-04 16:30:38 -0600

    Agregado tiendas y empezado a hacer modelos

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Clientes(models.Model):
-    nombre = models.CharField(max_length=50)
+class Usuarios(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    fecha_nac = models.DateField()
+    numero = models.IntegerField(max_length=25)
+    nacionalidad = models.CharField(max_length=20)
+
+class Direccion_Usuario(models.Model):
+	user = models.ForeignKey(User)
+	direccion = models.CharField(max_length=250)
+	estatus = models.CharField(max_length=10)
+	
